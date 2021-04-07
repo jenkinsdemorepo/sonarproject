401c2328b5660fd3ddec6e402dcfa6047e01da74
root <root@sonarmachine.c.devopsproject-001.internal>
2021-04-07 11:06:59 +0000

    new

---
+++ Feature.java
@@ -0,0 +1,11 @@
+public class Feature{
+// this is my comment
+// this is agin a comment
+// is thsi 
+	public static void main(String[] args) {
+//		System.out.println("Hello all");
+		LOGGER.log("feature")
+		LOGGER.log("dev")
+	}
+// this is final comment
+}
