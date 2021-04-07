18dba44caa56bc587110523e108f524e046909e5
root <root@sonarmachine.c.devopsproject-001.internal>
2021-04-07 10:05:07 +0000

    second

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class Hello{
+// this is my comment
+// this is agin a comment
+// is thsi 
 	public static void main(String[] args) {
 		System.out.println("Hello all");
 	}
+// this is final comment
 }
